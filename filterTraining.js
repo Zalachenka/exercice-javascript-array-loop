e2bef9a64ab50b3515aa38fe5ff714423f728a3a
Armony Saliou <armony.saliou@stackerine.com>
2019-10-24 17:02:25 +0200

    filterTraining.js and findTraining.js done, index changed to test

--- filterTraining.js
+++ filterTraining.js
@@ -1,6 +1,6 @@
 const floatingNumbers = [-7.2, 0.4, 0.8, -2.4, -96.9];
 function isNegative(n) {
-  return n < 0 && n > -5 ? Math.floor(n) : false;
+  return Math.floor(n) < 0 && Math.floor(n) > -5;
 }
 const newNumbers = floatingNumbers.filter(isNegative);
 console.log(newNumbers);
--- findTraining.js
+++ findTraining.js
@@ -1,5 +1,14 @@
 const users = ['Daisy', 'Donald', 'Beatrix', 'Fifi', 'Belinda'];
-function checkUsernames(name) {
-    return 
-    
-}
+/* other way
+const finalCheck = users.find(function(username) {
+  return username.substring(0, 2) === 'Be';
+});
+console.log(finalCheck);*/
+
+const finalCheck = users.find(function(usernames) {
+  return username[0] === 'B' && username[1] === 'e';
+});
+/* other way
+const finalCheck = users.find(function(username) {
+    return username.startsWith('Be')
+})
